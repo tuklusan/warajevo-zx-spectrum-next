d3e4883d4410e7dd5c9d33cafb13b86e3dca17a4
Supratim Sanyal <sanyal.duckdns.org.openvms.vax@gmail.com>
2026-08-24 23:37:10 -0400

    Cover injected IM0 prefixed opcodes

--- tests/wz_tests_placeholder.c
+++ tests/wz_tests_placeholder.c
@@ -2182,6 +2182,60 @@ int main(void)
     }
 
     if (wz_machine_init(&machine, profile) != WZ_RESULT_OK) {
+        fputs("machine reset before Z80 IM0 ED interrupt test failed\n", stderr);
+        return 1;
+    }
+    memset(&bus_log, 0, sizeof(bus_log));
+    wz_bus_observer_init(&bus_observer, record_bus_request, &bus_log);
+    wz_bus_input_init(&bus_input, read_bus_input, (void*)&interrupt_value);
+    interrupt_value = 0xedu;
+    machine.cpu.iff1 = 1u;
+    machine.cpu.iff2 = 1u;
+    machine.cpu.program_counter = 0x1234u;
+    machine.cpu.r = 0x7fu;
+    machine.cpu.main.a = 0x01u;
+    machine.memory[0x1234u] = 0x44u;
+    if (wz_machine_set_bus_observer(&machine, &bus_observer) != WZ_RESULT_OK ||
+        wz_machine_set_bus_input(&machine, &bus_input) != WZ_RESULT_OK ||
+        wz_z80_accept_maskable_interrupt(&machine) != WZ_RESULT_OK ||
+        machine.cpu.program_counter != 0x1235u || machine.cpu.main.a != 0xffu ||
+        machine.cpu.r != 0x01u || machine.master_tick != 22u || bus_log.count != 2u ||
+        bus_log.requests[0].cycle != WZ_BUS_INTERRUPT_ACKNOWLEDGE ||
+        bus_log.requests[1].cycle != WZ_BUS_M1_OPCODE_FETCH ||
+        bus_log.requests[1].master_tick != 14u || bus_log.requests[1].address != 0x1234u) {
+        fputs("Z80 IM0 injected ED opcode test failed\n", stderr);
+        return 1;
+    }
+
+    if (wz_machine_init(&machine, profile) != WZ_RESULT_OK) {
+        fputs("machine reset before Z80 IM0 DD interrupt test failed\n", stderr);
+        return 1;
+    }
+    memset(&bus_log, 0, sizeof(bus_log));
+    wz_bus_observer_init(&bus_observer, record_bus_request, &bus_log);
+    wz_bus_input_init(&bus_input, read_bus_input, (void*)&interrupt_value);
+    interrupt_value = 0xddu;
+    machine.cpu.iff1 = 1u;
+    machine.cpu.iff2 = 1u;
+    machine.cpu.program_counter = 0x1234u;
+    machine.cpu.r = 0x7fu;
+    machine.memory[0x1234u] = 0x21u;
+    machine.memory[0x1235u] = 0x78u;
+    machine.memory[0x1236u] = 0x56u;
+    if (wz_machine_set_bus_observer(&machine, &bus_observer) != WZ_RESULT_OK ||
+        wz_machine_set_bus_input(&machine, &bus_input) != WZ_RESULT_OK ||
+        wz_z80_accept_maskable_interrupt(&machine) != WZ_RESULT_OK ||
+        machine.cpu.program_counter != 0x1237u || machine.cpu.ix != 0x5678u ||
+        machine.cpu.r != 0x01u || machine.master_tick != 34u || bus_log.count != 4u ||
+        bus_log.requests[0].cycle != WZ_BUS_INTERRUPT_ACKNOWLEDGE ||
+        bus_log.requests[1].cycle != WZ_BUS_M1_OPCODE_FETCH ||
+        bus_log.requests[1].master_tick != 14u || bus_log.requests[1].address != 0x1234u ||
+        bus_log.requests[2].master_tick != 22u || bus_log.requests[3].master_tick != 28u) {
+        fputs("Z80 IM0 injected DD opcode test failed\n", stderr);
+        return 1;
+    }
+
+    if (wz_machine_init(&machine, profile) != WZ_RESULT_OK) {
         fputs("machine reset before Z80 IM1 interrupt test failed\n", stderr);
         return 1;
     }
