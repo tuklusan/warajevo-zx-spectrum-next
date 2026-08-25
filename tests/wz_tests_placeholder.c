c3448ab0b4f0970b4b430ace83a095cb48da58a6
Supratim Sanyal <sanyal.duckdns.org.openvms.vax@gmail.com>
2026-08-24 23:38:00 -0400

    Cover injected IM0 RST opcode

--- tests/wz_tests_placeholder.c
+++ tests/wz_tests_placeholder.c
@@ -2236,6 +2236,32 @@ int main(void)
     }
 
     if (wz_machine_init(&machine, profile) != WZ_RESULT_OK) {
+        fputs("machine reset before Z80 IM0 RST interrupt test failed\n", stderr);
+        return 1;
+    }
+    memset(&bus_log, 0, sizeof(bus_log));
+    wz_bus_observer_init(&bus_observer, record_bus_request, &bus_log);
+    wz_bus_input_init(&bus_input, read_bus_input, (void*)&interrupt_value);
+    interrupt_value = 0xffu;
+    machine.cpu.iff1 = 1u;
+    machine.cpu.iff2 = 1u;
+    machine.cpu.program_counter = 0x1234u;
+    machine.cpu.stack_pointer = 0x8000u;
+    machine.cpu.r = 0x7fu;
+    if (wz_machine_set_bus_observer(&machine, &bus_observer) != WZ_RESULT_OK ||
+        wz_machine_set_bus_input(&machine, &bus_input) != WZ_RESULT_OK ||
+        wz_z80_accept_maskable_interrupt(&machine) != WZ_RESULT_OK ||
+        machine.cpu.program_counter != 0x0038u || machine.cpu.stack_pointer != 0x7ffeu ||
+        machine.memory[0x7fffu] != 0x12u || machine.memory[0x7ffeu] != 0x34u ||
+        machine.cpu.r != 0u || machine.master_tick != 28u || bus_log.count != 4u ||
+        bus_log.requests[0].cycle != WZ_BUS_INTERRUPT_ACKNOWLEDGE ||
+        bus_log.requests[1].master_tick != 14u || bus_log.requests[2].master_tick != 16u ||
+        bus_log.requests[3].master_tick != 22u) {
+        fputs("Z80 IM0 injected RST opcode test failed\n", stderr);
+        return 1;
+    }
+
+    if (wz_machine_init(&machine, profile) != WZ_RESULT_OK) {
         fputs("machine reset before Z80 IM1 interrupt test failed\n", stderr);
         return 1;
     }
