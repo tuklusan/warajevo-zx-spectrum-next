4a6beca9fc599face0080d9346986e3aaa9f977c
Supratim Sanyal <sanyal.duckdns.org.openvms.vax@gmail.com>
2026-09-01 23:01:41 -0400

    \ Fix raster encoding header integration\

--- src/core/wz_raster.h
+++ src/core/wz_raster.h
@@ -1,8 +1,10 @@
+/*
 Warajevo ZX Spectrum Next
 Copyright (c) 2026 Supratim Sanyal, SANYALnet Labs, for new original project material.
 New original material is licensed under GNU GPL v2 or later (GPL-2.0-or-later), as stated in LICENSE.txt.
 Upstream Warajevo and third-party material retain their applicable copyrights and licenses.
 See LICENSE.txt and NOTICE.md for complete terms and provenance.
+*/
 
 #ifndef WZ_CORE_WZ_RASTER_H
 #define WZ_CORE_WZ_RASTER_H
@@ -22,7 +24,7 @@ static inline bool wz_raster_sample_is_valid(wz_byte_t sample)
 
 static inline bool wz_raster_sample_is_active(wz_byte_t sample)
 {
-    return sample >= WZ_RASTER_ACTIVE_MIN && sample <= WZ_RASTER_ACTIVE_MAX;
+    return sample <= WZ_RASTER_ACTIVE_MAX;
 }
 
 static inline bool wz_raster_sample_is_border(wz_byte_t sample)
