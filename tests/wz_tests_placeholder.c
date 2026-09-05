2d7644f85b39312ad868035dab0c0296858f5d68
Supratim Sanyal <sanyal.duckdns.org.openvms.vax@gmail.com>
2026-09-05 01:26:19 -0400

    "Fix-native-state-test-buffer-size-CR0208"

--- tests/wz_tests_placeholder.c
+++ tests/wz_tests_placeholder.c
@@ -2720,7 +2720,7 @@ int main(void)
     wz_machine_t machine;
     wz_machine_t restored;
     wz_scheduler_t scheduler;
-    wz_byte_t serialized[65609u];
+    wz_byte_t serialized[WZ_STATE_SNAPSHOT_CAPACITY];
     wz_machine_profile_t certified_profile;
     wz_state_writer_t writer;
     wz_qword_t first_hash;
