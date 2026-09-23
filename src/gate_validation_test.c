afa4cd4df811ace03354e49a1b49dc38428ff516
Supratim Sanyal <sanyal.duckdns.org.openvms.vax@gmail.com>
2026-09-22 23:16:07 -0400

    Remove temporary gate test

--- src/gate_validation_test.c
+++
@@ -1,5 +0,0 @@
-/* Copyright (c) 2026 Supratim Sanyal of SANYALnet Labs.
- * This file is governed by the SANYALnet Labs Non-Commercial License in the
- * root LICENSE file. Codex must be rejected by the unified push gate.
- */
-int gate_validation_test(void) { return 0; }
